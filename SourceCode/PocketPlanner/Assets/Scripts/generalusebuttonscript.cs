3692b8325b4b11bd6611ac6f0a7702fc7062e25c
WebNed <robert-ionut.nedelcu@students.plymouth.ac.uk>
2026-04-10 16:04:22 +0100

    WelcomeSceneDone
    Welcome Scene with connections to each separate menu

---
+++ SourceCode/PocketPlanner/Assets/Scripts/generalusebuttonscript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class generalusebuttonscript : MonoBehaviour
+{
+    public Button button;
+    public string targetScene;
+    public void buttonClicked()
+    {
+        Debug.Log("Button click");
+        SceneManager.LoadScene(targetScene);
+    }
+}
